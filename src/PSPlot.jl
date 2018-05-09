454a95ab2d8a452ec6e01e5ffcee2840a08806b2
reesepathak <rpathak@stanford.edu>
2018-05-08 17:07:07 -0700

    Homebrew fix

--- src/PSPlot.jl
+++ src/PSPlot.jl
@@ -1,5 +1,18 @@
 module PSPlot
 
+if !is_apple()
+    error("PSPlot currently only support OS X")
+end
+else
+    using Homebrew
+    brew_path = joinpath(Homebrew.prefix(), "bin")
+    ENV["PATH"] = string(ENV["PATH"], ":", brew_path)
+end
+
+function call_inkscape(cmd)
+    return run(`inkscape $cmd`)
+end
+
 using Reexport
 ENV["MATPLOTLIBRC"] = joinpath(Pkg.dir("PSPlot"), "assets")
 @reexport using PyPlot # makes all of PyPlot available
